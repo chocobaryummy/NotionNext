ef17cd945bdbde453e75e7226842c3037bf2c0b1
chocobaryummy <117326541+chocobaryummy@users.noreply.github.com>
2023-10-26 16:23:50 +0700

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -10,25 +10,25 @@ const CONFIG = {
   ],
 
   // 英雄区(首页顶部大卡)
-  HERO_TITLE_1: '分享编程',
-  HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
-  HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_1: 'Share programming',
+  HERO_TITLE_2: 'idea',
+  HERO_TITLE_3: "baarik's notes",
+  HERO_TITLE_4: 'New Version Online',
+  HERO_TITLE_5: 'NotionNext4.0 Easy customizable them',
+  HERO_TITLE_LINK: 'https://baarik.my.id',
 
   // 英雄区显示三个置顶分类
-  HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HERO_CATEGORY_1: { title: 'Must-see selection', url: '/tag/must-see-selection' },
+  HERO_CATEGORY_2: { title: 'Popular article', url: '/tag/popular article' },
+  HERO_CATEGORY_3: { title: 'Practice tutorial', url: '/tag/tutorial' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HERO_RECOMMEND_POST_TAG: '推荐',
+  HERO_RECOMMEND_POST_TAG: 'Recomended',
   HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   INFOCARD_GREETINGS: [
-    '你好！我是',
+    'Hi！Barik here.',
     '🔍 分享与热心帮助',
     '🤝 专修交互与设计',
     '🏃 脚踏实地行动派',
@@ -36,7 +36,7 @@ const CONFIG = {
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  INFO_CARD_URL: 'https://github.com/chocobaryummy/NotionNext', // 个人资料底部按钮链接
 
   // 用户技能图标
   GROUP_ICONS: [
